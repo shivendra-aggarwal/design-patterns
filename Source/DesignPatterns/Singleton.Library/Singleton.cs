fd5ae5d9595b6f2524fdf4894b58a40210148fb4
Shivendra Aggarwal <shivendra.aggarwal@outlook.com>
2018-04-16 15:55:14 +0530

    added singleton basic implementation

---
+++ Source/DesignPatterns/Singleton.Library/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Singleton.Library
+{
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton singleton = null;
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter Value " + counter.ToString());
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (singleton == null)
+                {
+                    singleton = new Singleton();
+                }
+                return singleton;
+            }
+        }
+        public void PrintMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ Source/DesignPatterns/Singleton.Run/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Singleton.Run
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Library.Singleton asEmployee = Library.Singleton.Instance;
+            asEmployee.PrintMessage("As Employee");
+
+            Library.Singleton asStudent = Library.Singleton.Instance;
+            asStudent.PrintMessage("As Student");
+
+            Console.ReadLine();
+        }
+    }
+}
